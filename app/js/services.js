8712b6f8cccaf9e18335a8377c4554a926eda12d
yahtaa <dmull01@gmail.com>
2014-08-08 19:39:55 -0400

    Remove Auth Factory

--- app/js/services.js
+++ app/js/services.js
@@ -3,23 +3,23 @@
 /* Services */
 
 
-// Demonstrate how to register services
-// In this case it is a simple value service.
+
 var app = angular.module('myApp.services', ['ngResource']);
+//Inventory Model
   app.factory('Inventory', ['$resource',
   function($resource){
     return $resource('inventory/:inventoryId.json', {}, {
       get: {method:'JSONP', params:{inventoryId:'inventories'}, isArray:true}
     });
   }]);
-
+//Scrap Model
   app.factory('Scrap', ['$resource', 
   	function($resource){
   		return $resource('scrap/:scrapId.json', {}, {
   			get: {method: 'JSONP', params:{scrapId:'scrap'}, isArray:true}
   		});
   	}]);
-
+//Messages Model
   app.factory('Messages', ['$resource',
   function($resource){
     return $resource('messages/:messageId.json', {}, {
@@ -28,21 +28,21 @@ var app = angular.module('myApp.services', ['ngResource']);
   }]);
 
   // USER AUTHENTICATION //
-  app.factory('Authentication', function($location) {
-  return {
-    login: function(credentials) {
-      if (credentials.email !== "admin@admin.com" || credentials.password !== "admin") {
-        toastr.error("Email must be 'admin@admin.com', and password must be 'admin'!");
-      } else {
-        $location.path('/main');
-        toastr.success("Hello " + credentials.email)
-      }
-    },
-    logout: function() {
-      $location.path('/');
-    }
-  };
-});
+//   app.factory('Authentication', function($location) {
+//   return {
+//     login: function(credentials) {
+//       if (credentials.email !== "admin@admin.com" || credentials.password !== "admin") {
+//         toastr.error("Email must be 'admin@admin.com', and password must be 'admin'!");
+//       } else {
+//         $location.path('/main');
+//         toastr.success("Hello " + credentials.email)
+//       }
+//     },
+//     logout: function() {
+//       $location.path('/');
+//     }
+//   };
+// });
 
 
 // FACTORY FOR SCRAP QUOTE //
